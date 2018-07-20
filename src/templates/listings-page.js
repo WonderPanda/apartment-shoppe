fd28406d65c0bae6bb634a581970b6cf9dbe1e8d
Jesse Carter <jcarter@carnegietechnologies.com>
2018-07-20 16:30:56 -0400

    Added Listings Page Components

---
+++ src/templates/listings-page.js
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Content, { HTMLContent } from '../components/Content';
+
+export const ListingsPageTemplate = ({ title, content, contentComponent }) => {
+  const PageContent = contentComponent || Content;
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <h1>Listings</h1>
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h2>
+              <PageContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+ListingsPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func
+};
+
+const ListingsPage = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <ListingsPageTemplate
+      contentComponent={HTMLContent}
+      title={post.frontmatter.title}
+      content={post.html}
+    />
+  );
+};
+
+ListingsPage.propTypes = {
+  data: PropTypes.object.isRequired
+};
+
+export default ListingsPage;
+
+export const listingPageQuery = graphql`
+  query ListingsPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
